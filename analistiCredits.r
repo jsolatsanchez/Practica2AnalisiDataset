3a60ed79d5ff3b8e5da14df77759285c530eb308
SANCHEZ, ANTONI <asanchez@imas.conselldemallorca.net>
2022-05-18 23:59:21 +0200

    Comprovació d'outliers

--- analistiCredits.r
+++ analistiCredits.r
@@ -22,3 +22,23 @@ linies <- dim(credit_ds)
 linies
 
 summary(credit_ds)
+
+# Analitzant la informació obtinguda de l'aplicació de la funció summary, es poden comprovar els valors mitjans i les medianes (i quartils)
+# de les diferents variables del dataset, d'aquesta anàlisi preliminar es poden extreure les següents conclusions:
+#   * El valor màxim de la variable quantitat es troba a una distància relativament superior de la mediana i la mitjana que el valor mínim, això pot indicar l'existència d'outliers
+#   * La resta de variables tenen una distribució de valors més propera la d'una normal.
+
+# A continuació s'analitzen els valors de la variable Quantitat per determinar si realment hi ha outliers.
+# a) Gràfics de caixes
+credit_ds_quantitat_bp <-boxplot(credit_ds$quantitat, main="Quantitat")
+credit_ds_quantitat_bp$out
+
+# b) Criteri de les dues desviacions típiques
+quantitat.outlier <- abs(scale(credit_ds$quantitat))
+quantitat.outlier
+
+
+# Comprovació de la normalitat
+ks.test(credit_ds$quantitat, pnorm, mean(credit_ds$quantitat), sd(credit_ds$quantitat))
+
+unique(unique(credit_ds$quantitat))
