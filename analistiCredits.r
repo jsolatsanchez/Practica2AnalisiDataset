80fef8ab1ef2ed6dfe7db7f2eeecaaf7c6c5b3d1
jsolaporta <solaporta.joan@gmail.com>
2022-05-26 19:09:17 +0200

    Afegir descriptius de les variables categòriques

--- analistiCredits.r
+++ analistiCredits.r
@@ -1,7 +1,7 @@
-# Càrrega de les llibreries emprades
+# Carrega de les llibreries emprades
 library(ggplot2)
 
-# Càrrega del dataset
+# Carrega del dataset
 credit_ds <- read.table(
     "https://archive.ics.uci.edu/ml/machine-learning-databases/statlog/german/german.data") # nolint
 attach(credit_ds)
@@ -15,18 +15,18 @@ colnames(credit_ds) <- c("estatCompteCorrent", "mesosCredit",
                     "edat", "altresPlans",
                     "propietatVivendaActual", "numCredits",
                     "tipusFeina",
-                    "numPersonesManteniment", "teTelèfon",
+                    "numPersonesManteniment", "telefon",
                     "estranger", "bonPagador")
 
 files <- dim(credit_ds)
 files
 
-# ANÀLISI PRELIMINAR =============================================================
+# ANALISI PRELIMINAR =============================================================
 summary(credit_ds)
 
 # Analitzant la informació obtinguda de l'aplicació de la funció summary, es poden comprovar els valors mitjans, les medianes i quartils.
-# de les diferents variables del dataset, d'aquesta anàlisi preliminar es poden extreure les següents conclusions:
-#   * El valor màxim de la variable quantitat es troba a una distància relativament superior de la mediana i la mitjana que el valor mínim, això pot indicar l'existència d'outliers
+# de les diferents variables del dataset, d'aquesta analisi preliminar es poden extreure les següents conclusions:
+#   * El valor maxim de la variable quantitat es troba a una distància relativament superior de la mediana i la mitjana que el valor mínim, això pot indicar l'existència d'outliers
 #   * La distribució de valors de les variables edat i mesosCredit també pot fer pensar que hi puguin haver outliers.
 #   * El camp 'bonPagador' està definit com a numèric (1-bon pagador i 2-mal pagador), per tant, es podria posar en termes de 0 i 1, per ser convertit a factor.
 
@@ -35,6 +35,101 @@ credit_ds$bonPagador[credit_ds$bonPagador==2] <-0
 # Converteix els valors numèrics a factors
 credit_ds$bonPagador <- as.factor(credit_ds$bonPagador)
 summary(credit_ds$bonPagador)
+#Converteix les codificacions a valors
+# Estat compte corrent
+credit_ds$estatCompteCorrent[credit_ds$estatCompteCorrent=='A11']<-'< 0DM'
+credit_ds$estatCompteCorrent[credit_ds$estatCompteCorrent=='A12']<-'[0DM,200DM]'
+credit_ds$estatCompteCorrent[credit_ds$estatCompteCorrent=='A13']<-'>= 200DM'
+credit_ds$estatCompteCorrent[credit_ds$estatCompteCorrent=='A14']<-'no checking account'
+credit_ds$estatCompteCorrent <- as.factor(credit_ds$estatCompteCorrent)
+
+#Historia de credits anterior
+credit_ds$historiaCreditsAnteriors[credit_ds$historiaCreditsAnteriors=='A30']<-'no credits taken/all credits paid back duly'
+credit_ds$historiaCreditsAnteriors[credit_ds$historiaCreditsAnteriors=='A31']<-'all credits at this bank paid back duly'
+credit_ds$historiaCreditsAnteriors[credit_ds$historiaCreditsAnteriors=='A32']<-'existing credits paid back duly till now'
+credit_ds$historiaCreditsAnteriors[credit_ds$historiaCreditsAnteriors=='A33']<-'delay in paying off in the past'
+credit_ds$historiaCreditsAnteriors[credit_ds$historiaCreditsAnteriors=='A34']<-'critical account/other credits existing (not at this bank)'
+credit_ds$historiaCreditsAnteriors <- as.factor(credit_ds$historiaCreditsAnteriors)
+
+#Motiu del credit
+credit_ds$motiu[credit_ds$motiu=='A40']<-'car (new)'
+credit_ds$motiu[credit_ds$motiu=='A41']<-'car (used)'
+credit_ds$motiu[credit_ds$motiu=='A42']<-'furniture/equipment'
+credit_ds$motiu[credit_ds$motiu=='A43']<-'radio/television'
+credit_ds$motiu[credit_ds$motiu=='A44']<-'domestic appliances'
+credit_ds$motiu[credit_ds$motiu=='A45']<-'repairs'
+credit_ds$motiu[credit_ds$motiu=='A46']<-'education'
+credit_ds$motiu[credit_ds$motiu=='A47']<-'(vacation - does not exist?)'
+credit_ds$motiu[credit_ds$motiu=='A48']<-'retraining'
+credit_ds$motiu[credit_ds$motiu=='A49']<-'business'
+credit_ds$motiu[credit_ds$motiu=='A410']<-'others'
+credit_ds$motiu <- as.factor(credit_ds$motiu)
+
+#Estat compte estalvi
+credit_ds$estatCompteEstalvi[credit_ds$estatCompteEstalvi=='A61']<-'< 100 DM'
+credit_ds$estatCompteEstalvi[credit_ds$estatCompteEstalvi=='A62']<-'[100DM,500DM)'
+credit_ds$estatCompteEstalvi[credit_ds$estatCompteEstalvi=='A63']<-'[500DM,1000DM)'
+credit_ds$estatCompteEstalvi[credit_ds$estatCompteEstalvi=='A64']<-'>= 1000 DM'
+credit_ds$estatCompteEstalvi[credit_ds$estatCompteEstalvi=='A65']<-'unknown/ no savings account'
+credit_ds$estatCompteEstalvi <- as.factor(credit_ds$estatCompteEstalvi)
+
+#Temps en el lloc de feina actual
+credit_ds$tempsTreballActual[credit_ds$tempsTreballActual=='A71']<-'unemployed'
+credit_ds$tempsTreballActual[credit_ds$tempsTreballActual=='A72']<-'< 1 year'
+credit_ds$tempsTreballActual[credit_ds$tempsTreballActual=='A73']<-'[1 year,4 years)'
+credit_ds$tempsTreballActual[credit_ds$tempsTreballActual=='A74']<-'[4 year,7 years)'
+credit_ds$tempsTreballActual[credit_ds$tempsTreballActual=='A75']<-'>= 7 years'
+credit_ds$tempsTreballActual <- as.factor(credit_ds$tempsTreballActual)
+
+#Sexe i estat civil
+credit_ds$sexeEstatCivil[credit_ds$sexeEstatCivil=='A91']<-'male : divorced/separated'
+credit_ds$sexeEstatCivil[credit_ds$sexeEstatCivil=='A92']<-'female : divorced/separated/married'
+credit_ds$sexeEstatCivil[credit_ds$sexeEstatCivil=='A93']<-'male : single'
+credit_ds$sexeEstatCivil[credit_ds$sexeEstatCivil=='A94']<-'male : married/widowed'
+credit_ds$sexeEstatCivil[credit_ds$sexeEstatCivil=='A95']<-'female : single'
+credit_ds$sexeEstatCivil <- as.factor(credit_ds$sexeEstatCivil)
+
+#Altres deutors
+credit_ds$altresDeutors[credit_ds$altresDeutors=='A101']<-'none'
+credit_ds$altresDeutors[credit_ds$altresDeutors=='A102']<-'co-applicant'
+credit_ds$altresDeutors[credit_ds$altresDeutors=='A103']<-'guarantor'
+credit_ds$altresDeutors <- as.factor(credit_ds$altresDeutors)
+
+#Propietats
+credit_ds$propietats[credit_ds$propietats=='A121']<-'real estate'
+credit_ds$propietats[credit_ds$propietats=='A122']<-'building society savings agreement/ life insurance'
+credit_ds$propietats[credit_ds$propietats=='A123']<-'car or other'
+credit_ds$propietats[credit_ds$propietats=='A124']<-'unknown/no property'
+credit_ds$propietats <- as.factor(credit_ds$propietats)
+
+#Altres plans
+credit_ds$altresPlans[credit_ds$altresPlans=='A141']<-'bank'
+credit_ds$altresPlans[credit_ds$altresPlans=='A142']<-'stores'
+credit_ds$altresPlans[credit_ds$altresPlans=='A143']<-'none'
+credit_ds$altresPlans <- as.factor(credit_ds$altresPlans)
+
+#Vivenda actual
+credit_ds$propietatVivendaActual[credit_ds$propietatVivendaActual=='A151']<-'rent'
+credit_ds$propietatVivendaActual[credit_ds$propietatVivendaActual=='A152']<-'own'
+credit_ds$propietatVivendaActual[credit_ds$propietatVivendaActual=='A153']<-'for free'
+credit_ds$propietatVivendaActual <- as.factor(credit_ds$propietatVivendaActual)
+
+#Feina
+credit_ds$tipusFeina[credit_ds$tipusFeina=='A171']<-'unemployed/ unskilled - non-resident'
+credit_ds$tipusFeina[credit_ds$tipusFeina=='A172']<-'unskilled - resident'
+credit_ds$tipusFeina[credit_ds$tipusFeina=='A173']<-'skilled employee / official'
+credit_ds$tipusFeina[credit_ds$tipusFeina=='A174']<-'management/ self-employed/highly qualified employee/ officer'
+credit_ds$tipusFeina <- as.factor(credit_ds$tipusFeina)
+
+#Telefon
+credit_ds$telefon[credit_ds$telefon=='A191']<-'none'
+credit_ds$telefon[credit_ds$telefon=='A192']<-'yes, registered under the customers name'
+credit_ds$telefon <- as.factor(credit_ds$telefon)
+
+#Treballador estranger
+credit_ds$estranger[credit_ds$estranger=='A201']<-'yes'
+credit_ds$estranger[credit_ds$estranger=='A202']<-'no'
+credit_ds$estranger <- as.factor(credit_ds$estranger)
 
 # ANÀLISI D'OUTLIERS ===========================================================
 # A continuació s'analitzen els valors de la variable Quantitat per determinar si realment hi ha outliers.
@@ -44,7 +139,7 @@ credit_ds_quantitat_bp$out
 
 # Tot i que la majoria de valors es concentren entre el rang 1.000 - 5.000, alguns valors estan per sobre dels 8.000, que poden ser considerats outliers.
 
-# b) Criteri de les dues desviacions típiques
+# b) Criteri de les dues desviacions tipiques
 quantitat_deteccio_outlier <- abs(scale(credit_ds$quantitat)) > 2 # Genera un array boolea per indicar si el valor de la posició x és outlier
 quantitat_deteccio_outlier
 
@@ -120,11 +215,15 @@ mesosCredit_netejat <- mesosCredit_deteccio_outlier[mesosCredit_deteccio_outlier
 mesosCredit_netejat <- mesosCredit_netejat$valor
 mesosCredit_netejat
 
+#Ens quedem amb el conjunt de dades net
+credit_ds_net<-credit_ds[credit_ds$quantitat_deteccio_outlier=='FALSE',]
+credit_ds_net<-credit_ds_net[credit_ds_net$edat_deteccio_outlier=='FALSE',]
+credit_ds_net<-credit_ds_net[credit_ds_net$mesosCredit_deteccio_outlier=='FALSE',]
 
 
 # COMPROVACIÓ DE LA NORMALITAT DE QUANTITAT ================================
 # a) Test de Kolmogorov-Smirnov
-ks.test(credit_ds$quantitat, pnorm, mean(credit_ds$quantitat), sd(credit_ds$quantitat))
+ks.test(credit_ds_net$quantitat, pnorm, mean(credit_ds_net$quantitat), sd(credit_ds_net$quantitat))
 # NOTA: Retorna un warning degut a què hi ha valors repetits a la sèrie
 ks.test(unique(credit_ds$quantitat), pnorm, mean(credit_ds$quantitat), sd(credit_ds$quantitat))
 
@@ -167,13 +266,14 @@ shapiro.test(mesosCredit_netejat)
 
 # En ambdós casos el p-valor és menor que el nivell de significació alfa=0,05, per tant no passa el test de normalitat
 
+# Test homoscedasticitat
 
 
 # COMPROVACIÓ DE VALORS BUITS, NULS I INDEFINITS
 # Valors nuls
-column(is.na(credit_ds))
+is.na(credit_ds_net)
 # Valors buits
-column(credit_ds=="")
+credit_ds_net==""
 
 # Del resultats obtinguts, es pot veure que no hi ha cap atribut que contingui instàncies amb valors nuls.
 
